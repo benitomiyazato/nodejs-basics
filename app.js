c71bb1f5d5f6e47dae455b93fae6fdf6822e1604
bemiyaz <benito.miya172@gmail.com>
2023-02-15 13:04:40 -0300

    callback hell into async await promises

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Testing nodemon")
+const { readFile, writeFile } = require("fs").promises;
+
+const readAndWrite = async () => {
+  console.log("start readandwrite");
+  const first = await readFile("./content/first.txt");
+  const second = await readFile("./content/second.txt");
+  await writeFile(
+    "./content/result-async.txt",
+    `Result of both op'sdasdasdsssss: ${first} ${second}`
+  );
+  console.log("end readandwrite");
+};
+
+console.log("start");
+
+readAndWrite();
+
+console.log("end");
